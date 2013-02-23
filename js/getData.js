af0d0f667ac7b9ffed0f94d6fd722a7255f4e308
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2013-02-23 15:10:02 +0200

    updated js

--- js/getData.js
+++ js/getData.js
@@ -1,10 +1,28 @@
-function getData(id,copii){
+var date=[["MACPN","MAE"],"MADR"];
+function findSum(array){
+	var sum=0;
+ 	for(var i=0;i<array.length;i++)
+ 		getData(array[0],0,function(data){
+ 			sum+=data
+ 		});
+ 	return sum;
+}
+function getData(array,copii,callback){
+	var data="institutie="
+	for(var i=0;i<array.length;i++)
+	{
+		data+=array[i]+",";
+	}
+	data[data.length]=0;
+	if(copii==1){
+		data += "&copii=1"
+	}
 	$.ajax({
 		dataType: "json",
 		url: "ajax/getData.php",
-		data: "minister="+id+"&copii="+copii,
+		data: data,
 		success: function(data){
-			console.log(data);
+			callback(data["results"][0]);
 		}
 	});
 }
