165c25421fd29cd4ae3bc8462c620191f75c4845
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2013-03-15 09:45:42 +0200

    Modified Ministere array Secretariatul General is now at Ministere

--- js/getData.js
+++ js/getData.js
@@ -1,5 +1,5 @@
-var ministere=[14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,35,54,65];
-var altele = [1,2,3,4,5,6,7,8,9,10,11,13,30,31,32,33,34,37,38,39,40,41,42,43,44,45,46,47,48,50,51,52,53];
+var ministere=[13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,35,54,65];
+var altele = [1,2,3,4,5,6,7,8,9,10,11,30,31,32,33,34,37,38,39,40,41,42,43,44,45,46,47,48,50,51,52,53];
 var levelMembers=[[ministere,altele],[ministere],[altele]];
 var levelNames=[["Ministere","Alte instituţii"],[]];
 var title=["Bugetul României","Ministerele României"];
